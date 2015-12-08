877c41f039b819f3fa3a88a3b8b28a411bfe351e
callmeknuts <callmeknuts@gmail.com>
2015-12-07 19:08:49 -0600

    1ST COMMIT 2nd Commit - near finished GUI prototype - not building DB's so far.

--- src/com/Mike/StudioMainMenu.java
+++ src/com/Mike/StudioMainMenu.java
@@ -2,6 +2,8 @@ package com.Mike;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Created by Mike on 12/7/2015.
@@ -16,10 +18,30 @@ public class StudioMainMenu extends JFrame {
     private JButton checkScheduleButton;
     private JButton bookSessionButton;
     private JButton quitButton;
+    private JButton addClientButton;
+    private JButton deleteClientButton;
+    private JButton viewClientsRecordsButton;
+    private JButton backToMenuButton;
+    private JButton returnToMenuButton;
+    private JPanel viewClients;
+    private JPanel addClients;
+    private JPanel deleteClients;
+    private JPanel clientViewer;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JButton zButton;
 
+    // Main menu card names
     private static String MAIN_MENU = "Main menu panel";
     private static String CLIENTS = "Client interaction panel";
     private  static String CALENDER = "Calender panel";
+    // Client panel - Client viewer card names
+    private static String VIEW_CLIENTS = "Client list";
+    private static String ADD_CLIENTS = "Client Text Inputs";
+    private static String DELETE_CLIENTS = "Remove Clients";
 
 
     StudioMainMenu(){
@@ -35,5 +57,52 @@ public class StudioMainMenu extends JFrame {
         rootPanel.add(mainMenuPanel, MAIN_MENU);
         rootPanel.add(clientPanel, CLIENTS);
         rootPanel.add(calenderPanel, CALENDER);
+        // Add cards to the client panel -> client view (allows the changing of the JPanel within the client panel to display the different formats
+//        clientPanel.add(clientViewer,ADD_CLIENTS);
+//        clientPanel.add(clientViewer,VIEW_CLIENTS);
+//        clientPanel.add(clientViewer,DELETE_CLIENTS);
+
+
+
+        quitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+        // BUTTON FUNCTION FROM MAIN MENU
+        addRemoveClientButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                CardLayout c1 = (CardLayout) rootPanel.getLayout();
+                c1.show(rootPanel, CLIENTS);
+
+            }
+        });
+        checkScheduleButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                CardLayout c2 = (CardLayout) rootPanel.getLayout();
+                c2.show(rootPanel,CALENDER);
+            }
+        });
+        // BUTTON FUNCTION FROM CALENDER
+        backToMenuButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                CardLayout c1 = (CardLayout)rootPanel.getLayout();
+                c1.show(rootPanel,MAIN_MENU);
+
+            }
+        });
+
+        // BUTTON FUNCTIONS FROM CLIENTS
+        returnToMenuButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                CardLayout c1 = (CardLayout)rootPanel.getLayout();
+                c1.show(rootPanel,MAIN_MENU);
+            }
+        });
     }
 }
